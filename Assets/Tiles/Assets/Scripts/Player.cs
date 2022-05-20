3d56c6470c8b4a0b12d4877f04602875352c40e5
Nikolya77777777777 <nikolya.cr@gmail.com>
2022-05-20 18:33:36 +0300

    добавив тайли і персонажа

---
+++ Assets/Tiles/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+
+    private bool isGrounded;
+    private Rigidbody2D rigidbody2d;
+    private void Start()
+    {
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+    private void FixedUpdate()
+    {
+        Vector3 position = transform.position;
+
+        position.x += Input.GetAxis("Horizontal") * speed;
+
+        transform.position = position;
+
+    }
+    private void Jump()
+    {
+        if (isGrounded)
+        {
+            isGrounded = false;
+            rigidbody2d.AddForce(transform.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+        public void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.tag == "Ground")
+            {
+                isGrounded = true;
+            }
+        }
+
+    }
+
+
